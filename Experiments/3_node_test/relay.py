6163312a902e29e7360603fa657d53fa9df0a254
Moh <mkashani.phd@gmail.com>
2025-04-13 20:46:42 -0500

    updated the relay

--- Experiments/3_node_test/relay.py
+++ Experiments/3_node_test/relay.py
@@ -85,7 +85,7 @@ def tx(conf:src.CONFIG, ROLE:str, phase:int = 2):
                     postamble = np.concatenate([conf.POSTAMBLE, [0 for _ in range(1000//conf.TX_SPS)]]),
                     scale = conf.TX_PAYLOAD_POWER_SCALE # send the payload with half the power of the preamble
                     )
-    if src.MQTT_TX(conf=conf, role=ROLE, phase=phase, verbose=1).wait_for_all_ready(sleep_time=1.5):
+    if src.MQTT_TX(conf=conf, role=ROLE, phase=phase, verbose=1).wait_for_all_ready(sleep_time=1):
         for i in range(10):
             tx.send_waveform(fsk_signal)
             time.sleep(0.1)
